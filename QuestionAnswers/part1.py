2dc7c7284b73102d13a11ea1700320b802ae1ff6
Paul Wissler <u0791342@utah.edu>
2021-09-18 10:51:28 -0600

    update tex with q1

--- QuestionAnswers/part1.py
+++ QuestionAnswers/part1.py
@@ -16,11 +16,13 @@ def q1a():
     x3 = np.array([1, 0, 1, 0, 1, 0, 0,])
     x4 = np.array([0, 0, 1, 1, 0, 0, 1,])
     y =  np.array([0, 0, 1, 1, 0, 0, 0,])
+    
     print('H(y): ', dtree.calc_entropy(dtree.calc_discrete_probability(y)))
     print('x1 gain:', dtree.calc_gain(x1, y))
     print('x2 gain:', dtree.calc_gain(x2, y))
     print('x3 gain:', dtree.calc_gain(x3, y))
     print('x4 gain:', dtree.calc_gain(x4, y))
+    
     # H(y):  0.863120568566631
     # x1 gain: 0.061743357932800724
     # x2 gain: 0.46956521111470695
@@ -29,14 +31,17 @@ def q1a():
     # x2 = x4, for maximum gain, so choose x2 as it comes first
     # if x2 = 1, then y = 0 (all x2 when 1 correspond to y when 0)
     # this results in the following data set, where x2 = 0
+    
     x1 = np.array([0, 0, 1,])
     x3 = np.array([1, 1, 0,])
     x4 = np.array([0, 1, 1,])
     y =  np.array([0, 1, 1,])
+    
     print('H(y): ', dtree.calc_entropy(dtree.calc_discrete_probability(y)))
     print('x1 gain:', dtree.calc_gain(x1, y))
     print('x3 gain:', dtree.calc_gain(x3, y))
     print('x4 gain:', dtree.calc_gain(x4, y))
+    
     # H(y):  0.9182958340544896
     # x1 gain: 0.2516291673878229
     # x3 gain: 0.2516291673878229
@@ -45,7 +50,7 @@ def q1a():
     # When x4 = 0 then y = 0, and when x4 = 1 then y = 1
     # Rules summary:
     # If x2 = 1, then y = 0
-    # If x2 = 0, then y = 0 if x4 = 0, and y = 0 if x4 = 1
+    # If x2 = 0, then y = 0 if x4 = 0, and y = 1 if x4 = 1
 
 
 def q2a():
